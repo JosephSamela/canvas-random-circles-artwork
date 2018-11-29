11d78e8395cdc462917d8e4e9096e94dbff2caf6
Joe Samela <jsamela@draper.com>
2018-11-29 09:24:59 -0500

    sized to viewport

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext("2d");
 var size = 3840;
 canvas.style.width = size + "px";
 canvas.style.height = size + "px";
-var scale = window.devicePixelRatio; // <--- Change to 1 on retina screens to see blurry canvas.
+var scale = window.devicePixelRatio;
 canvas.width = size * scale;
 canvas.height = size * scale;
 
@@ -12,34 +12,24 @@ canvas.height = size * scale;
 ctx.scale(scale, scale);
 var button = document.getElementById("refresh");
 
-var refresh = function() {
+var refresh = function () {
 
-ctx.clearRect(0,0,3840,3840);
-for(i=0; i<5000; i++) {
+    ctx.clearRect(0, 0, 3840, 3840);
+    for (i = 0; i < 5000; i++) {
 
-var x = Math.floor(Math.random()*3840);
-var y = Math.floor(Math.random()*3840);
-var radius = Math.floor(Math.random()*250);
+        var x = Math.floor(Math.random() * 3840);
+        var y = Math.floor(Math.random() * 3840);
+        var radius = Math.floor(Math.random() * 200);
 
-// cyan, red, green, blue, yello
-var colors = ["#51e1f5","#ff5c72","#72d9b2","#65a4f4", "#fee25e"];
+        // cyan, red, green, blue, yello
+        var colors = ["#51e1f5", "#ff5c72", "#72d9b2", "#65a4f4", "#fee25e"];
 
-var r = Math.floor(Math.random()*255);
-var g = Math.floor(Math.random()*255);
-var b = Math.floor(Math.random()*255);
-  
-ctx.beginPath();
-ctx.arc(x,y,radius,Math.PI*2,0,false);
-//ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
-ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-ctx.fill();
-ctx.closePath();
-  
-}
+        ctx.beginPath();
+        ctx.arc(x, y, radius, Math.PI * 2, 0, false);
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
 refresh();
-button.addEventListener("click", refresh, false);
-
-//setInterval(refresh, 60);
-
